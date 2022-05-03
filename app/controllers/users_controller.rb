eea1a63699798e4dd3e449e0d33e537f49f4ad19
shunsuke-chiba <hoshigame.297@gmail.com>
2022-05-03 08:38:06 +0000

    [Finish]ex-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+      @user =User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,14 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  has_many :relationships, class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
-  has_many :reverse_of_relationships, class_name:"Relationship",foreign_key:"followed",dependent: :destroy
-  
-  has_many :followings, throuth: :relationships,source: :followed
-  has_many :followers, through: :reverse_of_relationships,souce: :follower
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -21,5 +24,20 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+    #フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  #フォロー外す時の処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  #フォローしているか判定
+  def following?(user)
+    followings.include?(user)
   end
 end
