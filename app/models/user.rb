17f0aae9ba20a4c29735f70d1561827e78539e2f
shunsuke-chiba <hoshigame.297@gmail.com>
2022-05-05 03:24:41 +0000

    [Finish-Update]ex-5

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -25,19 +25,31 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
     #フォローした時の処理
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   #フォロー外す時の処理
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   #フォローしているか判定
   def following?(user)
     followings.include?(user)
+  end
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content+'%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%'+content)
+    else
+      User.where('name LIKE ?', '%'+content+'%')
+    end
   end
 end
